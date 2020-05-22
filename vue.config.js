9044dfda315222f28e41006f4cf41512537442ac
mehaotian <490272692@qq.com>
2020-05-22 20:22:03 +0800

    chore : 源码工程改为 HBuilderX 工程

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 
-const path = require('path')
-const CopyPlugin = require('./plugin')
+// const path = require('path')
+// const CopyPlugin = require('./plugin')
 
 module.exports = {
     configureWebpack: {
-        plugins: [new CopyPlugin({
-            from:path.join(__dirname,'src/pages/vue'),
-            to:path.join(__dirname,'src/pages/nvue')
-        })]
+        // plugins: [new CopyPlugin({
+        //     from:path.join(__dirname,'pages/vue'),
+        //     to:path.join(__dirname,'pages/nvue')
+        // })]
     }
 }
