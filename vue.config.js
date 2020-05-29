1ca466ccdad4a0ef96490dc790995b4ded036584
mehaotian <490272692@qq.com>
2020-05-29 11:49:48 +0800

    update : uni-popup 组件文档更新

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,9 @@ const CopyPlugin = require('./plugin')
 
 module.exports = {
     configureWebpack: {
-        // plugins: [new CopyPlugin({
-        //     from:path.join(__dirname,'pages/vue'),
-        //     to:path.join(__dirname,'pages/nvue')
-        // })]
+        plugins: [new CopyPlugin({
+            from:path.join(__dirname,'pages/vue'),
+            to:path.join(__dirname,'pages/nvue')
+        })]
     }
 }
